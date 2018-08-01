76980e2c4ae8f99030edec0767e117c92c040cf9
zungf <41270843+zungf@users.noreply.github.com>
2018-08-01 20:40:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1414,23 +1414,6 @@ const secre = [
 
 
 
-client.on('ready', function(){
-    var ms = 1000 ;
-    var setGame = [`^^he `,` ^^hel`,`^^help ^^invite];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/idk`);
-    }, ms);1000
-
-});
 
 
 
