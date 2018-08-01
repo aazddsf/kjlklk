9a024ec2961dde756b2170105eb8f22b6e6dd44b
zungf <41270843+zungf@users.noreply.github.com>
2018-08-02 01:33:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1484,6 +1484,35 @@ client.on("guildCreate", guild => {
 
 
 
+clien.on('message', message => {
+      if(message.content == '^^1 قران '){
+        message.react('🔊')}  return;
+  const ytdl = require('ytdl-core');
+    const stram0ptions = { seek: 0, volume: 100};  
+    const broadcast = 
+clien.createVoiceBroadcast();
+
+
+
+message.gulid.member(message.author).voiceChannel.join()
+       .then(connection => {
+           const stream = ytdl('https://www.youtube.com/watch?v=Ktync4j_nmA', { filter : 'audioonly'});
+           broadcast.playstream(stream);
+           const dispatcher = connection.playBroadcast(broadcast);
+ })
+.catch(console.error);
+});
+
+clien.on('messge', message => {
+       if(message.content == '^^قران 2'){
+           message.react('🔊')} return;
+    const ytdl = require('ytdl-core');
+      const stram0ptions = { seek: 0, volume:100};
+      const broadcast = 
+clien.createVoiceBroadcast();
+
+
+
 
 
 
