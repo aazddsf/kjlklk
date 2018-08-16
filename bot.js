fa92f531928f14bdaa3720d1678f56f48b4dc6c6
zungf <41270843+zungf@users.noreply.github.com>
2018-08-16 02:23:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1604,7 +1604,25 @@ client.on("message", message => {
 
 
 
+client.on('message', message => { //-MaX PicAssO#8266 codes©
+  if (message.content === ".id") {
+  let embed = new Discord.RichEmbed()//-MaX PicAssO#8266 codes©
+.setThumbnail(message.author.avatarURL)  
+.setAuthor(message.author.username)//-MaX PicAssO#8266 codes©
+.setDescription("**معلومات عن الحــساب**")
+            .setFooter(`-MaX PicAssO#8266.©`, 'codes.©')//-MaX PicAssO#8266 codes©
+.setColor("#9B59B6")
+.addField("**اســـم الحســاب**", `${message.author.username}`)//-MaX PicAssO#8266 codes©
+.addField('**تاق الحساب الخاص**', message.author.discriminator)
+.addField("**الرقـــم الشـــخصي**", message.author.id)//-MaX PicAssO#8266 codes©
+.addField('**بــــوت**', message.author.bot)
+.addField("**تاريخ التسجيل**", message.author.createdAt)//-MaX PicAssO#8266 codes©
+  //-MaX PicAssO#8266 codes©
 
+message.channel.sendEmbed(embed);
+ }
+});//تبغى تنشره حط حقوق كودز💙©
+  
 
 
 
