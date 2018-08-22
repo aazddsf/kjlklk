eef0d37ae3a660ef55c3206671dcf325c8af3d0c
zungf <41270843+zungf@users.noreply.github.com>
2018-08-22 16:08:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -337,7 +337,7 @@ client.on("message", message => {
 
 .invite | القسم الاول لي اضافه البوت 
 
-.support| القسم الثاني  الدعم الفني و المساعدة
+.supp| القسم الثاني  الدعم الفني و المساعدة
 
 القسم الثالث مصمم البوت | @ZinoDzYT#7947  
 
@@ -1438,7 +1438,7 @@ client.on("message", message => {
 
 .invite | القسم الاول لي اضافه البوت 
 
-.support| القسم الثاني  الدعم الفني و المساعدة
+.supp| القسم الثاني  الدعم الفني و المساعدة
 
 القسم الثالث مصمم البوت | @ZinoDzYT#7947  
 
@@ -1512,32 +1512,7 @@ message.author.sendEmbed(embed)
 
 
 
-client.on('message',message =>{
-    var prefix = ".";
-    if(message.content.startsWith(prefix + 'top')) {
-  message.guild.fetchInvites().then(i =>{
-  var invites = [];
-   
-  i.forEach(inv =>{
-    var [invs,i]=[{},null];
-     
-    if(inv.maxUses){
-        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
-    }else{
-        invs[inv.code] =+ inv.uses;
-    }
-        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
-   
-  });
-  var embed = new Discord.RichEmbed()
-  .setColor("#000000")
-  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
-           message.channel.send({ embed: embed });
-   
-  });
-   
-    }
-  });
+
 
 
 
