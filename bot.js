9403977b96dc84b39c5a1be33e56885b55af8459
zungf <41270843+zungf@users.noreply.github.com>
2018-08-24 22:14:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1656,7 +1656,27 @@ client.on('message', message => { //jackeo جاكيو
 
 
 
-	
+client.on('message',async msg => {
+  var p = ".";
+  if(msg.content.startsWith(p + "setdate")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`date :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Date : [${Year} - ${Month} - ${Dat}]]`);
+ },1000);
+  });
+  }
+ 
+});	
 	
 	
 	
