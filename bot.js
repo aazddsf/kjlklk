f69b27376d3f3e3ae9e7c947279f8eaada2d8ef0
zungf <41270843+zungf@users.noreply.github.com>
2018-09-04 20:05:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1997,8 +1997,8 @@ m.sendMessage(args)
 
 
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+
+
 const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
