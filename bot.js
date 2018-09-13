126df3021fa82066efda7120d3289b7e4fccd055
zungf <41270843+zungf@users.noreply.github.com>
2018-09-13 14:11:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2174,31 +2174,7 @@ client.on("ready", () => {
  
  
  
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("487269548010110987");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("487269548010110987");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
-});
+
 
 
 
