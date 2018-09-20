1ad1b053119726f86d213bc42c3f44f9882678f0
zungf <41270843+zungf@users.noreply.github.com>
2018-09-20 15:49:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2127,16 +2127,6 @@ m.sendMessage(args)
 
 
 
-client.on('message', message => {
-if (message.content.startsWith(prefix + 'tag')) {
-    let args = message.content.split(" ").slice(1);
-if(!args[0]) return message.reply('مرجو كتابة نص الدي تريد');  
-
-    figlet(args.join(" "), (err, data) => {
-              message.channel.send("```" + data + "```")
-           })
-}
-});
 
 
 
