40d2d719f96452708c6e761bfc9477bf89806f25
zungf <41270843+zungf@users.noreply.github.com>
2018-10-17 14:29:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4381,37 +4381,34 @@ client.on('message',async message => {
     });
 
 
-client.on('message',async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
-  let args = message.content.split(' ');
-  let tag;
-  if(args[0] === `${prefix}discrim`) {
-    if(args[1]) {
-      let discrim = Array.from(args[1]);
-      if(isNaN(args[1])) return message.channel.send(`- \`${message.author.username}\`, يجب ان تتكون هذه الخانة من ارقام وليس احرف`);
-      if(discrim.length !== 4) return message.channel.send(`- \`${message.author.username}\`, يجب ان يكون التاق مكون من 4 ارقام`);
 
-      tag = discrim.map(r => r.toString()).join('');
-      console.log(tag);
-      if(client.users.filter(f => f.discriminator === tag).size === 0) return message.channel.send(`- \`${message.author.username}\`, لا يوجد احد بهذا التاق`);
-      let iLD = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(client.users.filter(f => f.discriminator === tag).map(r => r.username).slice(0, 10).join('\n'))
-      .setFooter('By: xYouseeF\'₁₁ || Roýale.#0001');
-      message.channel.send(iLD);
-    } else if(!args[1]) {
-      tag = message.author.discriminator;
-      if(client.users.filter(f => f.discriminator === tag).size === 0) return message.channel.send(`- \`${message.author.username}\`, لا يوجد احد بهذا التاق`);
-      let L4U = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(client.users.filter(f => f.discriminator === tag).map(r => r.username).slice(0, 10).join('\n'))
-      .setFooter('By: xYouseeF\'₁₁ || Roýale.#0001');
-      message.channel.send(L4U);
+
+client.on('message', msg => {
+  if(msg.content.startsWith('$sug')) {
+    if(!msg.channel.guild) return msg.reply('**هذا الامر فقط للسيرفرات**').then(msg => msg.delete(3000));
+    if(!msg.guild.channels.find('name', 'suggestions')) return msg.reply('**الرجاء إضافة روم بإسم (suggestions)**');
+    let args = msg.content.split(" ").slice(1);
+  const embed = new Discord.RichEmbed()  
+      .setColor("#8325c0") 
+      .setDescription(`
+
+    if(!args[1]) return msg.reply('الرجاء كتابة الاقتراح').then(msg => msg.delete(3000));
+    //غيره على حسب اسم روم الاقتراحات او سوي مثل اسم الروم الموجود هنا
+    if(msg.guild.channels.find('name', '﴾-suggestions-﴿')) {
+      //غيره هنا كمان اذا غيرت فوق
+      msg.guild.channels.find('name', '﴾-suggestions-﴿').send(`
+    **﴾ ${msg.member} ﴿ تم الاقتراح من قبل**
+                        **[❖═════ الاقتراح ═══════❖]**
+                                             ﴾ @here ﴿
+**__${args.join(" ").split(msg.mentions.members.first()).slice(' ')}__**
+      `)
+      .then(function (message) {
+        message.react('✅')
+        message.react('❌')
+      })
+      }
     }
-  }
+
 });
-
-
 
 client.login(process.env.BOT_TOKEN);
