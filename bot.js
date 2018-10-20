b091cf001e7039a802e356c62579d9bda912ec5c
zungf <41270843+zungf@users.noreply.github.com>
2018-10-20 21:26:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4567,7 +4567,39 @@ message.guild.createChannel('Talk ◥', 'voice')
 
 
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!'
 
+client.on('message', function(message) {
+    const myID = "346454919328432128";
+    let args = message.content.split(" ").slice(1).join(" ");
+    if(message.content.startsWith(prefix + "changename")) {
+                if(message.author.id !== myID) return;
+            if(!args) return message.reply('لا تلعب في حساب غيرك ياشاطر');
+        client.user.setUsername(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "stream")) {
+                if(message.author.id !== myID) return;
+            if(!args) return message.reply('Streaming with ur sister');
+        client.user.setGame(args , 'https://twitch.tv/6xlez1%27);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "listen")) {
+                        if(message.author.id !== myID) return;
+            if(!args) return message.reply('listen music with ur sister ');
+        client.user.setActivity(args, {type:'LISTENING'});
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+    });
+    }
+});
 
 
 
