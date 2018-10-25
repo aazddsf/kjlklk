661874d1667e236e3e6706496e29871d83b81c3d
zungf <41270843+zungf@users.noreply.github.com>
2018-10-25 23:05:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -427,24 +427,6 @@ if (message.content.startsWith("#cv")) {
 
 
 
-   client.on("message", message => {
-    const prefix = "#"
-              
-          if(!message.channel.guild) return;
-   if(message.author.bot) return;
-      if(message.content === prefix + "image"){ 
-          const embed = new Discord.RichEmbed()
-  
-      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
-  .setAuthor(message.author.username, message.guild.iconrURL)
-    .setColor(0x164fe3)
-    .setImage(message.guild.iconURL)
-    .setURL(message.guild.iconrURL)
-                    .setTimestamp()
-
-   message.channel.send({embed});
-      }
-  });
   
   client.on('message', message => {
     if (message.author.x5bz) return;
