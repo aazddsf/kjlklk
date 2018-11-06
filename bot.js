028341d4a183c2376f30e22ecee1970c2a832163
Zino <41270843+zungf@users.noreply.github.com>
2018-11-06 13:46:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3738,7 +3738,7 @@ client.on('message', async msg => {
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 
-					.setFooter("Galaxy")
+					.setFooter("GalaxyBot")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 
 					
