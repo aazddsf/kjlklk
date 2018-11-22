199435e12a12113bfa2d143aac244be2dae69293
Zino <41270843+zungf@users.noreply.github.com>
2018-11-22 23:38:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3906,4 +3906,58 @@ client.on('guildMemberAdd', member => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('voiceStateUpdate', (old, now) => {
+  const channel = client.channels.get('ROOM ID');
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+  if (!size) return channel.setName(`Voice Online: [ ${currentSize} ]`);
+  if (currentSize !== size) channel.setName(`Voice Online: [ ${currentSize} ]`);
+});
+
+
+
+client.on ("guildMemberAdd", member => {
+  
+   var role = member.guild.roles.find ("name", "Only Games");
+   member.addRole (role);
+  
+})
+
+client.on ("guildMemberRemove", member => {
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
